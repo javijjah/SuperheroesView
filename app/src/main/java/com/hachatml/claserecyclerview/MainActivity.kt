639b14f0639eba1df3073a2334d1d8df7fbfef5c
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2023-11-24 19:14:23 +0100

    toast conseguido

--- app/src/main/java/com/hachatml/claserecyclerview/MainActivity.kt
+++ app/src/main/java/com/hachatml/claserecyclerview/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.hachatml.claserecyclerview
 
+import android.graphics.Paint.Align
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -18,11 +26,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hachatml.claserecyclerview.Views.SuperHero
 import com.hachatml.claserecyclerview.ui.theme.ClaseRecyclerViewTheme
 
@@ -38,8 +50,9 @@ class MainActivity : ComponentActivity() {
                 ) {
                     //Ejercicio 1
                     //SimpleRecyclerView()
-                    //Ejercicio 2
-                    SuperHeroView()
+                    //Ejercicio 2 hasta apartado 6
+                    //SuperHeroView()
+                    SuperHeroViewColumn()
                 }
             }
         }
@@ -68,8 +81,14 @@ fun SimpleRecyclerView() {
 }
 
 @Composable
-fun ItemHero(Superhero: Superhero) {
-    Card() {
+fun ItemHero(Superhero: Superhero, onItemSelected: (Superhero) -> Unit) {
+    Card(
+        modifier = Modifier
+            .width(200.dp)
+            .padding(20.dp)
+            .clickable { onItemSelected(Superhero) },
+        border = BorderStroke(2.dp, Color.Red)
+    ) {
         Column {
             Image(
                 painter = painterResource(id = Superhero.photo),
@@ -77,22 +96,32 @@ fun ItemHero(Superhero: Superhero) {
                 modifier = Modifier.fillMaxWidth(),
                 contentScale = ContentScale.Crop
             )
-            Text(text = Superhero.superheroName)
-            Text(text = Superhero.realName)
+            Text(text = Superhero.superheroName, modifier = Modifier.padding(10.dp))
+            Text(text = Superhero.realName, modifier = Modifier.padding(10.dp))
+            Text(
+                text = Superhero.publisher,
+                modifier = Modifier.padding(5.dp),
+                fontSize = 10.sp,
+                textAlign = TextAlign.Right
+            )
         }
     }
 }
 
 @Composable
 fun SuperHeroView() {
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)
+    var context = LocalContext.current
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(getSuperheroes()) {
-            ItemHero(Superhero = it)
+            ItemHero(Superhero = it
+            ) {
+                Toast.makeText(context, it.realName, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
-
 
 fun getSuperheroes(): MutableList<Superhero> {
     var SuperheroList = mutableListOf<Superhero>()
@@ -106,8 +135,21 @@ fun getSuperheroes(): MutableList<Superhero> {
     return SuperheroList
 }
 
+//Hasta aquí Ejercicio 2 hasta el apartado 6
+//Apartado7
+@Composable
+fun SuperHeroViewColumn() {
+    var context = LocalContext.current
+    LazyColumn(
+    ) {
+        items(getSuperheroes()) {
+            ItemHero(Superhero = it) {
+                Toast.makeText(context, it.realName, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
 
-//Ejercicio 2
 data class Superhero(
     var superheroName: String,
     var realName: String,
